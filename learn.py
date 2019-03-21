e9e5c33ee5c634c0716d9e0114d45b7670eb1723
rvkstudent <rvkstudent@yandex.ru>
2019-03-21 16:09:41 +0300

    Create learn.py

---
+++ learn.py
@@ -0,0 +1,71 @@
+import tkinter
+import datetime
+import dateutil.relativedelta
+#d2 = datetime.now() - dateutil.relativedelta.relativedelta(months=1)
+
+master = tkinter.Tk()
+canv_height = 300
+canv_width = 600
+x_period = 5
+candles_in_period = 6
+
+#<DATETIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>
+
+ohlc =[
+["20190319 100000", 206.40, 206.70, 205.93, 206.38, 7052240],
+["20190319 110000", 206.36, 207.41, 206.30, 207.22, 8528020],
+["20190319 120000", 207.22, 207.30, 206.78, 207.00, 4468400],
+["20190319 130000", 207.02, 207.45, 206.91, 207.30, 3210360],
+["20190319 140000", 207.26, 208.00, 207.23, 207.84, 7292400],
+["20190319 150000", 207.86, 208.31, 207.71, 208.01, 3895920],
+["20190319 160000", 208.05, 208.08, 206.95, 207.41, 5095890],
+["20190319 170000", 207.36, 207.65, 207.04, 207.41, 3215010],
+["20190319 180000", 207.42, 207.70, 206.87, 207.70, 4947180]
+]
+
+
+
+
+def get_extrem(ohlc):
+    maximum = 0
+    minimum = 10**6
+    for elem in ohlc:
+        if (elem[2] > maximum):
+            maximum = elem[2]
+        if (elem[3] < minimum):
+            minimum = elem[3]
+    return [maximum, minimum]
+
+price_coridor = get_extrem(ohlc)[0] - get_extrem(ohlc)[1]
+
+
+
+canvas = tkinter.Canvas(master, bg='white', height=canv_height, width=canv_width)
+
+grip_padding_top = 20
+grip_padding_bottom = 20
+grip_padding_right = 50
+grip_padding_left = 50
+
+area_pixels =  canv_height - grip_padding_bottom - grip_padding_top
+
+#  atr расчитать
+
+atr = 10
+
+pixels_in_price_step = area_pixels / atr
+
+print (pixels_in_price_step)
+
+canvas.create_line(grip_padding_left, grip_padding_top, grip_padding_left, canv_height-grip_padding_top, dash=(4, 2), fill='#A8A8A8')
+canvas.create_line(grip_padding_left, grip_padding_top, canv_width-grip_padding_right, grip_padding_top, dash=(4, 2), fill='#A8A8A8')
+canvas.create_line(canv_width-grip_padding_right, grip_padding_top, canv_width-grip_padding_right, canv_height-grip_padding_bottom, dash=(4, 2), fill='#A8A8A8')
+canvas.create_line(grip_padding_left, canv_height-grip_padding_bottom, canv_width-grip_padding_right, canv_height-grip_padding_bottom, dash=(4, 2), fill='#A8A8A8')
+
+
+#for i in range(5, canv_width, int(canv_width/x_period)):
+#    canvas.create_line(i, 5, i, canv_height, dash=(4, 2), fill='#A8A8A8')
+
+canvas.pack()
+master.mainloop()
+
